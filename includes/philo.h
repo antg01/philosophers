2945ebb33c2197c45cd1262d6d84b2380f3d6cc1
Antoine Gerard <angerard@fu-r9-p1.s19.be>
2024-09-10 13:40:45 +0200

    Makefile

---
+++ includes/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angerard <angerard@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 12:07:45 by angerard          #+#    #+#             */
+/*   Updated: 2024/09/10 12:19:36 by angerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_philo
+{
+	int	value;
+}		t_philo;
+
+typedef struct s_data
+{
+	int	value;
+}		t_data;
+
+#endif
